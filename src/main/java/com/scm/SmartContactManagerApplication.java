bd8efad7eb86d7ddd8059b06ea54a274e234084e
MrDeepeshSaxena <deepesh.saxenaa@gmail.com>
2024-06-11 00:43:38 +0530

    Created a Project - Smart Contact Manager

--- src/main/java/com/scm/SmartContactManagerApplication.java
+++ src/main/java/com/scm/SmartContactManagerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmartContactManagerApplication {
 
     public static void main(String[] args) {
-		System.out.println("Aplication Started");
+
 		SpringApplication.run(SmartContactManagerApplication.class, args);
     }
 
